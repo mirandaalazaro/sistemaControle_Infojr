16e260cc7c0a31c91e00730f09e9878e7dbb21f1
mirandaalazaro <mirandaalazaro@infojr.com.br>
2019-05-14 04:34:46 -0300

    Escopo do projeto - algoritmos dos recursos e estrutura básica do HTML, CSS e JavaScript

---
+++ javascript/dados.js
@@ -0,0 +1,25 @@
+
+// Adicionar uma pessoa ao sistema (não permitindo ter CPFs já cadastrados);
+function adicionarCidadao() {
+
+}
+// Ver apenas os que são maiores de idade;
+function selecionarAdultos() {
+
+}
+// Ver quais pessoas são de uma determinada UF;
+function filtrarPorUF() {
+
+}
+// Pesquisar pessoas pelo CPF;
+function pesquisarPorCPF() {
+
+}
+// Remover um cidadão da lista pelo CPF;
+function removerCidadao() {
+
+}
+// Ver quais pessoas que estão vivos e são crianças (considerem as que têm até 12 anos)
+function consultarCriancasVivas() {
+
+}
